1e09d49bbf578c7295ec6463dc7397e54dc370be
Crafty12345 <hunterdylan482@gmail.com>
2025-12-16 16:02:51 +0800

    Fixed a bug where the player's name would not display when sending messages

--- src/main/java/me/crafty/mcformatter/data/FormattedSentMessage.java
+++ src/main/java/me/crafty/mcformatter/data/FormattedSentMessage.java
@@ -2,19 +2,12 @@ package me.crafty.mcformatter.data;
 
 import me.crafty.mcformatter.McFormatterMod;
 import me.crafty.mcformatter.parser.ParserTree;
-import net.minecraft.datafixer.fix.TextComponentStrictJsonFix;
 import net.minecraft.network.message.MessageType;
 import net.minecraft.network.message.SentMessage;
 import net.minecraft.network.message.SignedMessage;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.*;
-
-import javax.swing.text.MutableAttributeSet;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
 
 // Parts of this were copied from
 // https://github.com/Patbox/StyledChat/blob/1.21.5/src/main/java/eu/pb4/styledchat/other/StyledChatSentMessage.java
@@ -31,12 +24,10 @@ public record FormattedSentMessage(SignedMessage message,
         SignedMessage signedMessage = message();
         ParserTree tree = new ParserTree();
         tree.parse(signedMessage.getSignedContent());
-        System.out.println("Content=" + signedMessage.getSignedContent());
-        //Text txt = Text.literal(tree.toString());
-        MutableText txt = tree.toText();
-
-        McFormatterMod.LOGGER.info("Output: " + tree.toString());
-        //McFormatterMod.LOGGER.info("sending message");
+        String senderName = pSender.getDisplayName().getString();
+        String prefix = "<" + senderName + "> ";
+        MutableText txt = Text.literal(prefix);
+        txt.append(tree.toText());
         pSender.networkHandler.sendChatMessage(signedMessage, MessageType.params(McFormatterMod.MESSAGE_TYPE_ID, pSender.server.getRegistryManager(), txt));
     }
 }
